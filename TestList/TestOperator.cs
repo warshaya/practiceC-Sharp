4570a3f01bdec92b8adf1e22ee1d2a10bab6f9c0
warshaya <aaron.warshay@gmail.com>
2017-08-02 19:06:43 -0400

    trying to debug the Operator stack overflow and segfault

--- TestList/TestOperator.cs
+++ TestList/TestOperator.cs
@@ -3,12 +3,16 @@ class TestOperator
    public static void Main()
    {
       List<int> a = new List<int>();
-      a.Add( 1 );
-      a.Add( 2 );
+//      a.Add( 1 );
+//      a.Add( 2 );
       List<int> b = new List<int>();
-      b.Add( 1 );
-      b.Add( 2 );
-      System.Console.WriteLine( a == b );
+//      b.Add( 1 );
+//      b.Add( 2 );
+      if ( a.Equals(b) ) 
+      {
+      System.Console.WriteLine( "works for null");
+      }
+//      System.Console.WriteLine( a == b );
 //      b.Add( 3 );
 //      System.Console.WriteLine( a == b );
    }
